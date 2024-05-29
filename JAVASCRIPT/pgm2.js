542bb8d47c4315ec00d921f819eda8227fd3c7d6
anjali216 <anjalik216@gmail.com>
2024-05-29 14:47:41 +0530

    first commit

---
+++ JAVASCRIPT/pgm2.js
@@ -0,0 +1,28 @@
+var name="anjali";
+console.log(name)
+console.log(typeof name)
+var age=20
+console.log(typeof age)
+
+var isMinor=true
+console.log(typeof isMinor)
+
+var profession;
+console.log(typeof profession)
+
+
+console.log(typeof x)
+//By default js assign x to a variable while compiling
+var carName= null;
+console.log(typeof carName)
+
+
+var fruits=['mango','apple','kiwi']
+console.log(typeof fruits)
+
+var obj={
+    name:  'anjali',
+    age :20,
+    location: 'kakkanad'  
+}
+console.log(typeof obj)
